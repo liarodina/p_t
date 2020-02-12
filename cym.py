8ae05274491cedce4d28af57b51228d4df8f2dee
lia <rodinafla@yandex.ru>
2020-02-12 18:13:50 +0300

    проверка цума
    нихера не понятно

---
+++ cym.py
@@ -0,0 +1,71 @@
+{
+  "id": "43df2aaf-2b36-4551-ba6d-905f129796e0",
+  "version": "2.0",
+  "name": "cym",
+  "url": "https://github.com",
+  "tests": [{
+    "id": "5ccbe856-d7a5-40c5-bb5f-dd5b136317bb",
+    "name": "Untitled",
+    "commands": [{
+      "id": "4dfbab69-3ab8-4f75-824e-1c3854ad7a37",
+      "comment": "",
+      "command": "open",
+      "target": "/liarodina/python_training",
+      "targets": [],
+      "value": ""
+    }, {
+      "id": "b4beb90f-6faa-4f2f-b614-d5cefec19ae8",
+      "comment": "",
+      "command": "setWindowSize",
+      "target": "550x692",
+      "targets": [],
+      "value": ""
+    }, {
+      "id": "8f53b0c2-5550-4136-9129-6b058ffb9f4c",
+      "comment": "",
+      "command": "mouseOver",
+      "target": "css=.octicon-git-branch > path",
+      "targets": [
+        ["css=.octicon-git-branch > path", "css:finder"]
+      ],
+      "value": ""
+    }, {
+      "id": "6edd4f5b-4b69-43f8-baa1-2ff549524c6c",
+      "comment": "",
+      "command": "mouseOut",
+      "target": "css=.octicon-git-branch > path",
+      "targets": [
+        ["css=.octicon-git-branch > path", "css:finder"]
+      ],
+      "value": ""
+    }, {
+      "id": "8c2047e3-3373-4661-bde3-d92f4b7e481b",
+      "comment": "",
+      "command": "mouseOver",
+      "target": "css=.octicon-git-branch",
+      "targets": [
+        ["css=.octicon-git-branch", "css:finder"]
+      ],
+      "value": ""
+    }, {
+      "id": "f610358c-3e9d-41f2-a1b4-04537333cead",
+      "comment": "",
+      "command": "mouseOut",
+      "target": "css=.octicon-git-branch",
+      "targets": [
+        ["css=.octicon-git-branch", "css:finder"]
+      ],
+      "value": ""
+    }]
+  }],
+  "suites": [{
+    "id": "ccbd43dc-5d73-4765-84a7-517a19fb85e2",
+    "name": "Default Suite",
+    "persistSession": false,
+    "parallel": false,
+    "timeout": 300,
+    "tests": ["5ccbe856-d7a5-40c5-bb5f-dd5b136317bb"]
+  }],
+  "urls": ["https://github.com/"],
+  "plugins": []
+}
